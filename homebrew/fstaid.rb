8baacb4597417be01ce083aa851888662d9aa5f3
winebarrel <sgwr_dts@yahoo.co.jp>
2017-04-10 21:42:55 +0900

    Update formula sha256 [ci skip]

--- homebrew/fstaid.rb
+++ homebrew/fstaid.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Fstaid < Formula
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   homepage 'https://github.com/winebarrel/fstaid'
   url "https://github.com/winebarrel/fstaid/releases/download/v#{VERSION}/fstaid-v#{VERSION}-darwin-amd64.gz"
-  sha256 'pkg/fstaid-v0.1.0-darwin-amd64.gz'
+  sha256 '16a93005c30dc54640512de1635959fa94507e1b45e0af3ef90fa847a9618554'
   version VERSION
   head 'https://github.com/winebarrel/fstaid.git', :branch => 'master'
 
