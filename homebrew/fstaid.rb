60641cbd8e53ac131b745617afe8d6a5910cfc00
Genki Sugawara <sugawara@cookpad.com>
2017-04-12 13:11:43 +0900

    Fix homebrew formula sha256 [ci skip]

--- homebrew/fstaid.rb
+++ homebrew/fstaid.rb
@@ -5,7 +5,7 @@ class Fstaid < Formula
 
   homepage 'https://github.com/winebarrel/fstaid'
   url "https://github.com/winebarrel/fstaid/releases/download/v#{VERSION}/fstaid-v#{VERSION}-darwin-amd64.gz"
-  sha256 '27089a6b6dcdd0fbfe3c25e05909fb8b892afa4b6d25bdda144b0aebf4c3c6ab'
+  sha256 'c717a479b8726773f0fd17ccb37052f25c708db4599bc4f0a50f23449271cc71'
   version VERSION
   head 'https://github.com/winebarrel/fstaid.git', :branch => 'master'
 
